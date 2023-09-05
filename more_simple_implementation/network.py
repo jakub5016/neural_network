04fe4fb1f82641b0e255adfeaacb4ec2c4d5068f
jakub5016 <me.jakub.gil@gmail.com>
2023-09-05 14:21:04 +0200

    Basic matrix multiply

--- more_simple_implementation/network.py
+++ more_simple_implementation/network.py
@@ -1,18 +1,26 @@
-import numpy as np
 from random import random
+import numpy as np 
+network_size = [2,3,3]  
+
+neurons = []
+weights = []
+biases = []
+
+for index, item in enumerate(network_size):
+    neurons.append(np.array([0] * item))
+
+    if index > 0:
+        weights.append(np.array([[random()]*item, [random()] * item]))
+        biases.append(np.array([[random()]*item, [random()] * item]))
+    else:
+        weights.append([])
+        biases.append([])
 
 
-network_size =  [2, 2, 3]
+neurons[0] = np.array([1,1])
 
-neurons = np.empty([len(network_size), max(network_size)]) 
-
-for index, elem in enumerate(neurons):
-    print(network_size[index])
-    elem[network_size[index]:] = 0
-
-
-print(neurons)
-
-
-
+# What we want to multiply 
+print(neurons[0])
+print(weights[1])
+print(np.matmul(neurons[0], weights[1]))
 
