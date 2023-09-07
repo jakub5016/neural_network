dd840f122c022ba81c401721b0da694abf600026
jakub5016 <me.jakub.gil@gmail.com>
2023-09-07 17:23:50 +0200

    evaluate func

--- more_simple_implementation/network.py
+++ more_simple_implementation/network.py
@@ -21,11 +21,16 @@ class Network():
                 self.biases.append(np.array([[random()]*item] * self.network_size[index-1]))
             else:
                 self.weights.append(np.array([]))
-                self.biases.append([])
+                self.biases.append(np.array([]))
 
         for index, item in enumerate(self.neurons):
             if index > 0:
                 item = np.matmul(self.neurons[index-1], self.weights[index])
+
+        print("NEURONS:", self.neurons)
+        print("WEIGHTS: ", self.weights)
+        print("BIASES:", self.biases)
+
 
     def save(self):
         weights_as_lists = [weight.tolist() for weight in self.weights]
@@ -33,6 +38,31 @@ class Network():
         with open('weights.json', 'w') as f:
             json.dump(weights_as_lists, f)
 
+    def evaluate(self, input):
 
-network = Network(network_size=[2,2,3])
-network.mount()
+        if len(input) == len(self.neurons[0]):
+            self.neurons[0] = input 
+
+        else:
+            print("Wrong input size")
+            return -1
+        
+        for index, layer in enumerate(self.neurons):
+            if index > 0:
+                layer = np.matmul(self.neurons[index-1], self.weights[index])
+
+                print(layer)
+                
+                for elem_index, element in enumerate(layer):
+                    for bias_list in self.biases[index]:
+                        element = element + bias_list[elem_index]
+
+
+                    element = np.tanh(element)
+
+
+
+if __name__ == "__main__":
+    network = Network(network_size=[2,2,3])
+    network.mount()
+    network.evaluate([2,2])
