aef9b786f8016f2d17c0b70445439893057a186d
jakub5016 <me.jakub.gil@gmail.com>
2023-09-10 19:56:42 +0200

    Bias gradient

--- more_simple_implementation/network.py
+++ more_simple_implementation/network.py
@@ -1,6 +1,5 @@
 from random import random
 import numpy as np 
-import pandas as pd
 import json
 
 class Network():
@@ -18,7 +17,7 @@ class Network():
 
             if index > 0:
                 self.weights.append(np.array([[random()]*item] * self.network_size[index-1]))
-                self.biases.append(np.array([[random()]*item] * self.network_size[index-1]))
+                self.biases.append(np.array([1] * item))
             else:
                 self.weights.append(np.array([]))
                 self.biases.append(np.array([]))
@@ -27,10 +26,13 @@ class Network():
             if index > 0:
                 item = np.matmul(self.neurons[index-1], self.weights[index])
 
-        print("NEURONS:", self.neurons)
-        print("WEIGHTS: ", self.weights)
-        print("BIASES:", self.biases)
+        self.print_network_status()
 
+
+    def print_network_status(self):
+        print("\nNEURONS:", self.neurons)
+        print("WEIGHTS: ", self.weights)
+        print("BIASES:", self.biases, "\n")
 
     def save(self):
         weights_as_lists = [weight.tolist() for weight in self.weights]
@@ -52,24 +54,30 @@ class Network():
             if layer_index > 0:
                 self.neurons[layer_index] = np.matmul(self.neurons[layer_index-1], self.weights[layer_index])
                 for elem_index in range(len(self.neurons[layer_index])):
-                    for i in self.biases[layer_index]:
-                        self.neurons[layer_index][elem_index] += i[elem_index]
-                        self.neurons[layer_index][elem_index] = np.tanh(self.neurons[layer_index][elem_index])
+                    self.neurons[layer_index][elem_index] += self.biases[layer_index][elem_index]
+                    
+                    # Use softmax on last index
+                    # if layer_index ==(len(self.neurons) -1):
+                    #     self.neurons[layer_index][elem_index] = np.exp(self.neurons[layer_index][elem_index])/ np.sum(np.exp(self.neurons[layer_index][elem_index]), dtype=np.float64)
+                    # else:
+                    self.neurons[layer_index][elem_index] = np.tanh(self.neurons[layer_index][elem_index])
                 
         return self.neurons[-1]
     
     def back_propagation(self, answer):
         if len(answer) == len(self.neurons[-1]):
             cost = np.square(answer - self.neurons[-1])
-            print(f"Cost: {cost}")
+            print(f"Cost: {cost} \n")
 
             for index_L in range(len(self.neurons[-1])):
                 for index_L_1 in range(len(self.neurons[-2])):
                     weight_gradient = 2*self.neurons[-2][index_L_1] * np.tan(self.neurons[-1][index_L]) * (self.neurons[-1][index_L] - answer[index_L])
                     print(f"Weight gradient for neurons: {index_L} in last layer, {index_L_1} in pervous layer ::: {weight_gradient}")
+                    bias_gradient = weight_gradient/self.neurons[-2][index_L_1] 
+                    print(f"Bias gradient ::: {bias_gradient}, devided by: {self.neurons[-2][index_L_1] }")
 
 if __name__ == "__main__":
     network = Network(network_size=[2,2])
     network.mount()
-    print(f"For [1,1] input: {network.evaluate([1,2])}")
+    print(f"For [1,1] input: {network.evaluate([1,2])} \n")
     network.back_propagation(np.array([1, 0]))
